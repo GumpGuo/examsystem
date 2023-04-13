678695e53260c84d9f31fcbc5216d689111ac794
guoyebin <guoyebin@hikvision.com.cn>
2023-04-13 16:36:04 +0800

    [add]提交项目初始化工程文件。

---
+++ src/test/java/cn/cfyt/examsystem/ExamsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cfyt.examsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
